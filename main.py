4e19a3751df1b88190c75b8203c588c68d158b0c
Shariq Torres <enshenpa@gmail.com>
2022-07-01 15:56:46 -0700

    updates

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import requests
+import base64
+import os
 
 app = Flask(__name__)
 @app.route('/')
 def start():
     return render_template("index.html")
+
+@app.route("/redirect")
+def oauth_redirect():
+    clientID= os.environ.get('CLIENT_ID', '')
+    clientSecret = os.environ.get('CLIENT_SECRET','')
+    client_secret_combined = clientID + ":" + clientSecret
+    client_secret_combined_bytes = client_secret_combined.encode('ascii').b64encode(client_secret_combined)
+    client_secret_combined_str = client_secret_combined_bytes.decode('ascii')
+    code = request.args.key('code', None)
+    data = {'code': code, 'grant_type': 'authorization_code', 'redirect_uri': 'https://7zljzja270.execute-api.us-west-1.amazonaws.com/me'}
+    headers = {'Authorization': client_secret_combined_str }
+    res = requests.post('https://zoom.us/oauth/token', data=payload, headers=headers)
+    if res.status_code == 200:
+        access_token_data = res.json()
+        return render_template("debug.html", data=access_token_data)
+
 
 app.run()
