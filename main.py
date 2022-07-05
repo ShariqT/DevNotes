4baa1ec1f72f6f5a915a6eb6dfddafa2f4201694
Shariq Torres <enshenpa@gmail.com>
2022-07-05 15:24:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def start():
     res.headers['crossOriginEmbedderPolicy'] = False
     res.headers['Strict-Transport-Security'] = 'max-age=31536000'
     res.headers['X-Content-Type-Options'] = 'nosniff'
-    res.headers['Content-Security-Policy'] = "default-src 'self'; styleSrc 'self'; scriptSrc 'self' appssdk.zoom.us; imgSrc 'self'; connect-src 'self'; base-uri 'self'; form-action 'self'"
+    res.headers['Content-Security-Policy'] = "default-src 'self' appssdk.zoom.us; style-src 'self'; script-src 'self' appssdk.zoom.us; img-src 'self'; connect-src 'self'; base-uri 'self'; form-action 'self'"
     return res
 
 @app.route("/redirect")
