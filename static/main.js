aa90d3dbab98480ba563071db28c952aad25664e
Shariq Torres <enshenpa@gmail.com>
2022-07-05 15:30:56 -0700

    updates

---
+++ static/main.js
@@ -0,0 +1,8 @@
+async function configureApp() {
+    const configResponse = await zoomSdk.config({
+      version: "0.16",
+      popoutSize: {width: 480, height: 360},
+      capabilities: ["shareApp", "getMeetingParticipants"]
+    })
+    console.log(configResponse)
+  }
